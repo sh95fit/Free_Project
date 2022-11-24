910b6ebac7a68881e22ba25a67a5ad3648d659ec
sh95fit <h95fit@gmail.com>
2022-11-24 16:41:52 +0900

    SK Thingplug 데이터 수집 프로그램(device_Id 추출)

---
+++ Prac3_Lora_data_collection/DeviceID_Extraction.py
@@ -0,0 +1,24 @@
+import requests
+import urllib.request
+import xml.etree.ElementTree as ET
+import re
+from Thingplug_info import Thingplug_info as info
+
+
+
+
+
+
+# Lora 디바이스 검색 (API - searchMyDevice 활용)
+division = "searchDevice"
+function = "myDevice"
+searchDevice_url = f"{info.ThingplugApiUrl}/{info.X_M2M_Origin}?division={division}&function={function}&startIndex=1&countPerPage=500"
+print(searchDevice_url)  
+req = requests.get(searchDevice_url, headers=info.hdr)
+data = re.findall('(?<=<device_Id>)(\w{0,})(?=<\/device_Id>)', req.text)
+
+device_Id = []
+for i in data :
+    device_Id.append(i)
+
+print(device_Id)
---
+++ Prac3_Lora_data_collection/Thingplug_info.py
@@ -0,0 +1,9 @@
+class Thingplug_info :
+    ThingplugApiUrl = "https://thingplugpf.sktiot.com:9443"
+    locale = "ko"
+    uKey = "Y05LVHFSekh2TWVGa3N6U2pnTWFoUGI3SFdDekc3S0g1ZXNOWTgzdG8xZncvWTJaTVVZd2E3WVV1Q0VTM3grYQ=="
+    X_M2M_Origin = "ThingPlug"
+    X_M2M_RI = "12345"
+    
+    #헤더 테스트 미적용
+    hdr={'Accept' : "application/xml", 'locale' : locale, 'uKey' : uKey, 'X-M2M-Origin' : X_M2M_Origin, 'X-M2M-RI' : X_M2M_RI}
