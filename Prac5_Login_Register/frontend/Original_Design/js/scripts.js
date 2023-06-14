80d7976cac6bc68d2236366814ef15bdb11bd8dc
sh95fit <h95fit@gmail.com>
2023-06-14 17:19:20 +0900

    미디어쿼리 적용(반응형), 동적 효과 적용(JS, ScrollReveal, Typed JS)

--- Prac5_Login_Register/frontend/Original_Design/js/scripts.js
+++ Prac5_Login_Register/frontend/Original_Design/js/scripts.js
@@ -37,3 +37,26 @@ menuIcon.onclick = () => {
   navbar.classList.toggle('active');
 };
 
+
+//scroll reveal
+ScrollReveal({
+  // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+
+
+// Typed JS
+var typed = new Typed('.multiple-text', {
+  strings: ['프로젝트 매니저', '백엔드 개발자', '프론트엔드 개발자'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
