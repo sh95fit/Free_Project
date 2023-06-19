6cc3607b188b1336fa19bd040e5ea01397e1a518
sh95fit <h95fit@gmail.com>
2023-06-19 13:58:30 +0900

    에러코드 상태 정의 수정 (Hex 적용)

---
+++ Prac6_SocketTestTool/multi_err/data_packet.py
@@ -0,0 +1,53 @@
+import socket
+
+def send_data(ip_addr, port, modem, type, mppt, err, com, count):
+  type_status = {"단상":"01", "3상":"02"}
+  com_status = {"정상":"00", "통신 실패":"39"}
+  err_status = {"01":"0002", "02":"0004", "03":"0008", "04":"0010", "05":"0020", "06":"0040", "07":"0080", "08":"0100", "09":"0200", "10":"0400", "11":"0800", "12":"1000", "13":"0101", "14":"0141", "15":"000B"}
+  station_num = f"{count:02}"
+  data = ""
+
+  # 데이터 타입 선정
+  print(f"{modem} {type} {mppt} {err} {com}")
+
+  pre_set_lora = f"1401{type_status[type]}{station_num}{com_status[com]}"
+  pre_set_lte = f"011111111111111111111111111111111111111111{station_num}1401{type_status[type]}{station_num}{com_status[com]}"
+  end_set = f"xxxxxxxxxxxxxxxx{err_status[err]}"
+
+  if modem=="Lora" and type=="단상" and mppt=="미포함" :
+    data = pre_set_lora + f"aaaabbbbccccddddeeeeffff03e80258" + end_set
+  elif modem=="Lora" and type=="단상" and mppt=="포함" :
+    data = pre_set_lora + f"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccddddeeeeffff03ef0258" + end_set
+  elif modem=="Lora" and type=="3상" and mppt=="미포함" :
+    data = pre_set_lora + f"aaaabbbbccccccccddddeeeeffffgggghhhhiiiijjjjjjjj03ef0258"+ end_set
+  elif modem=="Lora" and type=="3상" and mppt=="포함" :
+    data = pre_set_lora + f"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccddddeeeeffffgggghhhhiiiijjjjjjjj03ef0258" + end_set
+  elif modem=="LTE" and type=="단상" and mppt=="미포함" :
+    data = pre_set_lte + f"aaaabbbbccccddddeeeeffff03e80258" + end_set
+  elif modem=="LTE" and type=="단상" and mppt=="포함" :
+    data = pre_set_lte + f"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccddddeeeeffff03ef0258" + end_set
+  elif modem=="LTE" and type=="3상" and mppt=="미포함" :
+    data = pre_set_lte + f"aaaabbbbccccccccddddeeeeffffgggghhhhiiiijjjjjjjj03ef0258"+ end_set
+  elif modem=="LTE" and type=="3상" and mppt=="포함" :
+    data = pre_set_lte + f"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccddddeeeeffffgggghhhhiiiijjjjjjjj03ef0258" + end_set
+  else :
+    pass
+
+  # 소켓 통신
+  try :
+    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client_socket.connect((ip_addr, int(port)))
+    print("서버와 연결되었습니다.")
+
+    try :
+      client_socket.send(data.encode())
+      print(data, len(data))
+      # print(station_num)
+      print("데이터가 정상적으로 전송되었습니다.")
+    except :
+      print("데이터 전송에 실패했습니다.")
+
+    client_socket.close()
+
+  except ConnectionRefusedError :
+    print("서버와 연결할 수 없습니다.")
