f3945ec1bdcf59e429171cab9bd7442187be5991
sh95fit <h95fit@gmail.com>
2023-10-20 11:14:20 +0900

    개인서버, GSREMS서버 DB 연결 및 Alembic 이중화(main_migrations, gsrems_migrations)

--- Prac7_Data_Pipeline/main.py
+++ Prac7_Data_Pipeline/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Depends, Path
 from models import main_models
+from models.gsrems_models import gsmon_solar_data
 from datetime import datetime
 from database.connection import main_db, gsrems_db
 from sqlalchemy.orm import Session
+from sqlalchemy import and_, or_, desc
 
 
 app = FastAPI()
@@ -25,3 +27,12 @@ async def select_data(rtu_id: int = Path(..., title='rtu_id'), db: Session = Dep
         return {"message": "Raw data not found"}
 
     return {"data": data}
+
+
+# GSREMS 정의된 테이블 활용하기
+@app.get("/select_gsrems/{rtu_id}")
+async def gsrems_data(rtu_id: int = Path(..., title='rtu_id'), db: Session = Depends(gsrems_db)):
+    result = db.query(gsmon_solar_data).filter(
+        and_(gsmon_solar_data.c.rtu_id == rtu_id, gsmon_solar_data.c.save_time_id == now_date)).order_by(desc(gsmon_solar_data.c.save_time)).all()
+
+    return result
