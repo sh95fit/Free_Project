5a4ff5f68438f40f576ddecfeb606ec42fba20cc
sh95fit <h95fit@gmail.com>
2023-12-19 17:28:41 +0900

    CORS 이슈 조치 (localhost, 127.0.0.1 인식 차이로 접근 URL에 따른 미허용 이슈)

--- Prac8_Call_Procedure/backend/main.py
+++ Prac8_Call_Procedure/backend/main.py
@@ -13,7 +13,8 @@ def main():
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 띄우는 리액트 앱의 주소에 맞게 수정
+    allow_origins=["http://localhost:3000",
+                   "http://127.0.0.1:3000"],  # 띄우는 리액트 앱의 주소에 맞게 수정
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
