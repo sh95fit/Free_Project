c30d6d1b22bf28d3faba841e5f248ca3f167b637
sh95fit <h95fit@gmail.com>
2024-04-05 14:30:40 +0900

    MQTT Broker-Subscriber-Publisher 코드 작성

---
+++ Prac12_MQTT/MQTT_Client/publisher.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as mqtt
+import time
+
+# MQTT 브로커 정보
+broker_address = "127.0.0.1"  # 브로커의 IP 주소
+port = 1883  # MQTT 브로커의 포트
+topic = "dev1"  # 토픽 이름
+
+# MQTT 클라이언트 설정
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+# MQTT 브로커에 연결
+client.connect(broker_address, port)
+
+# 메시지를 보낼 데이터
+data_to_send = {
+    "inv": 123,
+    "ina": 456,
+    "inp": 789
+}
+
+# 10초에 한 번씩 메시지를 보내는 루프
+while True:
+    # 보낼 메시지 생성
+    message = f"inv={data_to_send['inv']}, ina={data_to_send['ina']}, inp={data_to_send['inp']}"
+
+    # 토픽에 메시지 발행
+    client.publish(topic, message)
+
+    # 메시지 발행 정보 출력
+    print("Message sent:", message)
+
+    # 10초 대기
+    time.sleep(30)
--- Prac12_MQTT/main.py
+++ Prac12_MQTT/main.py
@@ -1,70 +1,184 @@
-from fastapi import FastAPI, WebSocket, BackgroundTasks, Request
-from pymongo import MongoClient
-import paho.mqtt.client as mqtt
+# from fastapi import FastAPI, WebSocket, Request, BackgroundTasks
+# from fastapi.responses import HTMLResponse
+# from fastapi.templating import Jinja2Templates
+# from pymongo import MongoClient
+# import paho.mqtt.client as mqtt
+# from dotenv import dotenv_values, load_dotenv
+
+# load_dotenv()
+
+# # docker run -d --name mqtt-mongodb -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=1234 -p 27017:27017 mongo
+# # docker run -d --name mqtt-broker -p 1883:1883 -p 9001:9001 mqtt-broker
+
+# # MQTT Broker 및 FastAPI 설정
+# config = dotenv_values(
+#     r"C:\Users\user\Desktop\KIMSEHUN\develop\Free_Project\Prac12_MQTT\.env")
+# app = FastAPI()
+# templates = Jinja2Templates(
+#     directory=r"C:\Users\user\Desktop\KIMSEHUN\develop\Free_Project\Prac12_MQTT\templates")
+
+# # MongoDB 클라이언트 설정
+# client = MongoClient(config['MONGO_URI'])
+# db = client[config['MONGO_DB']]
+# collection = db[config['MONGO_COLLECTION']]
+
+# # MQTT 브로커 정보
+# broker_address = config['MQTT_BROKER_ADDRESS']
+# port = int(config['MQTT_BROKER_PORT'])
+# topic = config['MQTT_TOPIC']
+
+# # MQTT 클라이언트 설정
+# mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+
+# def on_message(client, event, msg_id, message):
+#     payload = message.payload.decode()
+#     data = {
+#         "topic": message.topic,
+#         "inv": payload['inv'],
+#         "ina": payload['ina'],
+#         "inp": payload['inp']
+#     }
+#     collection.insert_one(data)
+
+
+# # MQTT 클라이언트에 메세지 수신 콜백 함수 등록
+# mqtt_client.on_message = on_message
+
+# # MQTT 브로커 연결
+# mqtt_client.connect(broker_address, port)
+
+# # MQTT 클라이언트가 브로커의 특정 주제를 구독하도록 설정
+# mqtt_client.subscribe(topic)
+
+
+# # 백그라운드 작업 : MQTT 클라이언트 실행
+# def run_mqtt_client():
+#     mqtt_client.loop_forever()
+
+
+# # 웹소켓 클라이언트 : 실시간 데이터 전송
+# @app.websocket("/ws")
+# async def websocket_endpoint(ws: WebSocket):
+#     await ws.accept()
+#     while True:
+#         latest_data = collection.find_one(sort=[('_id', -1)])
+#         if latest_data:
+#             await ws.send_json(latest_data)
+
+# # 홈페이지 렌더링
+
+
+# @app.get("/", response_class=HTMLResponse)
+# async def root(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
+
+
+# # 백그라운드 작업 실행
+# background_tasks = BackgroundTasks()
+# background_tasks.add_task(run_mqtt_client)
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+################################################################
+from fastapi import FastAPI, WebSocket, Request, BackgroundTasks, WebSocketState
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from dotenv import dotenv_values
+from pymongo import MongoClient
+import paho.mqtt.client as mqtt
+from dotenv import dotenv_values, load_dotenv
+import asyncio
+
+load_dotenv()
+
+# MQTT Broker 및 FastAPI 설정
+config = dotenv_values(
+    r"C:\Users\user\Desktop\KIMSEHUN\develop\Free_Project\Prac12_MQTT\.env")
+app = FastAPI()
+templates = Jinja2Templates(
+    directory=r"C:\Users\user\Desktop\KIMSEHUN\develop\Free_Project\Prac12_MQTT\templates")
 
 # MongoDB 클라이언트 설정
-config = dotenv_values(".env")
 client = MongoClient(config['MONGO_URI'])
 db = client[config['MONGO_DB']]
 collection = db[config['MONGO_COLLECTION']]
-
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
 # MQTT 브로커 정보
 broker_address = config['MQTT_BROKER_ADDRESS']
 port = int(config['MQTT_BROKER_PORT'])
 topic = config['MQTT_TOPIC']
 
-# MQTT 클라이언트 설정
+# 웹소켓 클라이언트 목록
+websocket_clients = set()
 
 
-def on_message(client, userdata, message):
-    # MQTT 메시지 수신 시 실행되는 콜백 함수
-    payload = message.payload.decode()
-    data = {
-        "topic": message.topic,
-        "inv": payload['inv'],
-        "ina": payload['ina'],
-        "inp": payload['inp']
-    }
-    collection.insert_one(data)
-
-
-mqtt_client = mqtt.Client(
-    callback_api_version=mqtt.CallbackAPIVersion.VERSION1)
-mqtt_client.on_message = on_message
-mqtt_client.connect(broker_address, port)
-mqtt_client.subscribe(topic)
-
-# 백그라운드 작업: MQTT 클라이언트 실행
-
-
+# 백그라운드 작업 : MQTT 클라이언트 실행
 def run_mqtt_client():
+    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+    def on_message(client, event, msg_id, message):
+        payload = message.payload.decode()
+        data = {
+            "topic": message.topic,
+            "inv": payload['inv'],
+            "ina": payload['ina'],
+            "inp": payload['inp']
+        }
+        collection.insert_one(data)
+
+        # 데이터를 수신하면 모든 WebSocket 클라이언트에게 전송
+        for ws in websocket_clients:
+            asyncio.create_task(send_data_to_websocket(ws, data))
+
+    # MQTT 클라이언트에 메세지 수신 콜백 함수 등록
+    mqtt_client.on_message = on_message
+
+    # MQTT 브로커 연결
+    mqtt_client.connect(broker_address, port)
+
+    # MQTT 클라이언트가 브로커의 특정 주제를 구독하도록 설정
+    mqtt_client.subscribe(topic)
+
+    # MQTT 클라이언트 실행
     mqtt_client.loop_forever()
 
 
+async def send_data_to_websocket(ws, data):
+    await ws.send_json(data)
+
+
+# 웹소켓 클라이언트 : 실시간 데이터 전송
 @app.websocket("/ws")
 async def websocket_endpoint(ws: WebSocket):
     await ws.accept()
-    while True:
-        # MongoDB에서 최근 데이터 가져오기
-        latest_data = collection.find_one(sort=[('_id', -1)])
-        if latest_data:
-            await ws.send_json(latest_data)
+    websocket_clients.add(ws)
+    try:
+        while True:
+            # 연결이 끊어지면 루프 종료
+            if ws.client_state != WebSocketState.CONNECTED:
+                break
+            await asyncio.sleep(0.1)
+    finally:
+        websocket_clients.remove(ws)
 
 
+# 홈페이지 렌더링
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
+async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 # 백그라운드 작업 실행
 background_tasks = BackgroundTasks()
 background_tasks.add_task(run_mqtt_client)
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
