eb61fdf10911b80ef579faa55a0b845e131e3432
sh95fit <h95fit@gmail.com>
2024-04-26 14:54:16 +0900

    sched 모듈을 활용한 파이썬 스케줄러 적용 테스트

--- Prac14_Modem/RTU/RTU.py
+++ Prac14_Modem/RTU/RTU.py
@@ -18,7 +18,7 @@ logger = Logger(logDirectory, 'a')
 
 conf = Conf()
 mqtt = MQTT(logger)
-serial = SerialManager(conf.rs485,logger)
+serial = SerialManager(conf.rs485, logger)
 inverter = Inverter(conf.rs485, logger)
 
 if hasattr(conf, "rs485"):
@@ -38,10 +38,30 @@ inverter_type = input("Inverter Type DIP >> ").strip()
 # 인버터 목록 가져오기
 ivt_list = inverter.getSerialNumber(inverter_type)
 
-# 인버터 데이터 가져오기
-res = inverter.readData(inverter_type, port, ivt_list)
 
-# 인버터 데이터 보내기
-for r in res:
-    mqtt.sendData(r)
+# 스케줄러 생성
+scheduler = sched.scheduler(time.time, time.sleep)
 
+
+def fetch_and_send_data():
+    # 인버터 데이터 가져오기
+    res = inverter.readData(inverter_type, port, ivt_list)
+
+    # 인버터 데이터 보내기
+    for r in res:
+        mqtt.sendData(r)
+
+
+def schedule_task():
+    # 1분마다 작업 실행
+    scheduler.enter(60, 1, schedule_task)
+
+    # 데이터 가져오고 보내기
+    fetch_and_send_data()
+
+
+# 초기 작업 스케줄링
+schedule_task()
+
+# 스케줄러 실행
+scheduler.run()
